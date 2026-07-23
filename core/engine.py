84e6d2d4564b892c513e30ab1792e191c8bb2a00
slumpkid10 <cypranx@gmail.com>
2026-07-23 14:10:42 +0100

    Connect Reddit hunter to engine

--- core/engine.py
+++ core/engine.py
@@ -2,9 +2,14 @@
 VERIDEX Core Engine
 """
 
+from hunters.reddit_hunter import RedditHunter
+
+
 class VeridexEngine:
     def __init__(self):
         self.status = "READY"
+        self.reddit = RedditHunter()
 
     def start(self):
         print("🚀 VERIDEX Engine Started")
+        self.reddit.scan()
